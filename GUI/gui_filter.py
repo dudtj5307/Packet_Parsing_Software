23852a9e80bccfd2c05ce81966bb513cfeb73915
yslee <yslee5307@gmail.com>
2025-03-30 21:46:43 +0900

    utils/filter_proxy_model added & caching changed

--- GUI/gui_filter.py
+++ GUI/gui_filter.py
@@ -13,6 +13,7 @@ class FilterWidget(QWidget, Ui_FilterForm):
         self.setupUi(self)
 
         self.parent = parent
+        if parent: self.parent.destroyed.connect(self.close)
 
         # Create the checkbox items in the scroll area's layout
         self.create_items(data_set)
@@ -45,7 +46,7 @@ class FilterWidget(QWidget, Ui_FilterForm):
 
     def create_items(self, data_set):
         for item in data_set:
-            checkbox = QCheckBox(parent=self.parent)
+            checkbox = QCheckBox()
             checkbox.setChecked(True)
             checkbox.setText(item)
             self.verticalLayout.addWidget(checkbox)
@@ -54,20 +55,21 @@ class FilterWidget(QWidget, Ui_FilterForm):
         self.verticalLayout.addItem(spacerItem)
 
         # Update widget size based on its content
-        self.scrollArea.setMinimumHeight(self.widget.sizeHint().height() + 25)
+        scrollbox_height = min(self.widget.sizeHint().height() + 25, 130)
+        self.scrollArea.setMinimumHeight(scrollbox_height)
 
 
 class FilterHeaderView(QHeaderView):
-    def __init__(self, orientation, table_view, parent=None):
+    def __init__(self, orientation, parent=None):
         super().__init__(orientation, parent)
-        self.table_view = table_view
+        self.parent = parent
+        self.table_view = parent.table_csv
         self.setSectionsClickable(True)
         self.current_col = None
         self.filter_popup = None
         self.setAutoFillBackground(True)
         self.setStyleSheet("QHeaderView::section { background-color: rgb(240, 240, 240); }")
-        # For saving unique values
-        self.unique_values = defaultdict(lambda: False)
+        self.filter_setting = {}
 
     def contextMenuEvent(self, event):
         if self.filter_popup:
@@ -75,17 +77,20 @@ class FilterHeaderView(QHeaderView):
 
         # Get current column & table model
         self.current_col = self.logicalIndexAt(event.pos())
+
         model = self.table_view.model()
+        # get data from origin model
+        origin_model = model.sourceModel()
 
         # Unique values from selected column
-        self.unique_values = defaultdict(lambda: False)
-        for row in range(model.rowCount()):
-            index = model.index(row, self.current_col)
-            value = model.data(index, Qt.ItemDataRole.DisplayRole)
-            self.unique_values[value] = True
+        unique_values = defaultdict(lambda: False)
+        for row in range(origin_model.rowCount()):
+            index = origin_model.index(row, self.current_col)
+            value = origin_model.data(index, Qt.ItemDataRole.DisplayRole)
+            unique_values[value] = True
 
         # Pop up Filter UI as Dialog
-        self.filter_popup = FilterWidget(self.unique_values, self.table_view)
+        self.filter_popup = FilterWidget(unique_values, self.parent)
         self.filter_popup.setWindowFlags(Qt.WindowType.Dialog | Qt.WindowType.CustomizeWindowHint)
 
         # Connect signals for the apply and close buttons
@@ -99,18 +104,29 @@ class FilterHeaderView(QHeaderView):
 
     def apply_filter(self):
         # Find all QCheckBox widgets within the popup and get their texts if checked
-        selected_values = {cb.text() for cb in self.filter_popup.findChildren(QCheckBox) if cb.isChecked()}
+        filtered_values = [cb.text() for cb in self.filter_popup.findChildren(QCheckBox) if not cb.isChecked()]
         model = self.table_view.model()
 
-        # Hide rows that do not match the selected values
-        for row in range(model.rowCount()):
-            index = model.index(row, self.current_col)
-            value = model.data(index, Qt.ItemDataRole.DisplayRole)
-            self.table_view.setRowHidden(row, value not in selected_values)
+        model.setFilterForColumn(self.current_col, filtered_values)
+        # # Hide rows that do not match the selected values
+        # for row in range(model.rowCount()):
+        #     index = model.index(row, self.current_col)
+        #     value = model.data(index, Qt.ItemDataRole.DisplayRole)
+        #     self.table_view.setRowHidden(row, value in filtered_values)
 
         # TODO: After applying - add "color"? or indicator
         # Close the popup after applying the filter
         # self.filter_popup.close()
+        self.save_filter_settings(self.filter_setting)
+
+    def set_filter_setting(self, filter_setting):
+        self.filter_setting = filter_setting
+
+    def save_filter_settings(self, filter_setting):
+        self.parent.cache[self.parent.current_csv_path]['filter_setting'] = filter_setting
+        self.set_filter_setting(filter_setting)
+        pass
+
 
 
 if __name__ == "__main__":
--- GUI/gui_viewer.py
+++ GUI/gui_viewer.py
@@ -6,6 +6,7 @@ from PyQt6.QtGui import QIcon, QBrush, QColor
 from PyQt6.QtCore import Qt
 
 from utils.viewer.table_model import CSVTableModel
+from utils.viewer.filter_model import CSVFilterProxyModel
 from utils.viewer.csv_loader import CSVLoaderThread
 from utils.viewer.search_model import SearchModel
 
@@ -30,9 +31,12 @@ class ViewerWindow(QMainWindow, Ui_ViewerWindow):
         self.load_csv_list()
 
         # CSV Table Cache
-        self.cache = {}
+        self.cache = defaultdict(lambda: {'table_model': None, 'table_data': None,
+                                          'search_model': None, 'search_setting':[],
+                                          'filter_model': None, 'filter_setting':[],})
 
-        # Signal set
+        # CSV List
+        self.current_csv_path = None
         self.list_csv_names.clicked.connect(self.clicked_csv_list)
 
         # Search Widget
@@ -44,7 +48,7 @@ class ViewerWindow(QMainWindow, Ui_ViewerWindow):
         self.frame_search.setVisible(False)
 
         # Custom horizontal header with filtering
-        self.table_csv.setHorizontalHeader(FilterHeaderView(Qt.Orientation.Horizontal, self.table_csv))
+        self.table_csv.setHorizontalHeader(FilterHeaderView(Qt.Orientation.Horizontal, self))
 
         # CSV Table default size
         self.table_csv.horizontalHeader().setDefaultSectionSize(80)     # cell width
@@ -93,6 +97,11 @@ class ViewerWindow(QMainWindow, Ui_ViewerWindow):
         self.get_csv_name[csv_path] = csv_name
         self.list_csv_names.addItem(csv_name)
 
+    def paint_list_csv(self, csv_path, color):
+        csv_name = self.get_csv_name[csv_path]
+        for item in self.list_csv_names.findItems(csv_name, Qt.MatchFlag.MatchExactly):
+            item.setBackground(QBrush(QColor(color[0], color[1], color[2])))  # Green
+
     def load_csv_list(self):
         csv_file_names = os.listdir(self.csv_folder_path)
         if len(csv_file_names) == 0:
@@ -104,15 +113,22 @@ class ViewerWindow(QMainWindow, Ui_ViewerWindow):
                 self.add_item(csv_name, csv_path)
 
     def clicked_csv_list(self):
+        # Close Search Widget
         self.search_widget_hide()
+        # Close Filter Widget
+        header = self.table_csv.horizontalHeader()
+        if hasattr(header, "filter_popup") and header.filter_popup:
+            header.filter_popup.close()
+
         csv_name = self.list_csv_names.currentItem().text()
         csv_path = self.get_csv_path[csv_name]
+        self.current_csv_path = csv_path
+
         # Check if already in cache
         if csv_path in self.cache:
-            self.update_table(self.cache[csv_path], csv_path)
+            self.update_table(csv_path)
         else:
-            for item in self.list_csv_names.findItems(csv_name, Qt.MatchFlag.MatchExactly):
-                item.setBackground(QBrush(QColor(220, 220, 220)))
+            self.paint_list_csv(csv_path, (220, 220, 220))  # Gray
             self.loader_thread = CSVLoaderThread(csv_path)
             self.loader_thread.load_complete.connect(self.csv_load_complete)
             self.loader_thread.load_failed.connect(self.csv_load_failed)
@@ -120,36 +136,38 @@ class ViewerWindow(QMainWindow, Ui_ViewerWindow):
 
     def csv_load_complete(self, csv_path, data):
         # Save in cache
-        self.cache[csv_path] = data
-
-        # Paint Green to list_csv
-        csv_name = self.get_csv_name[csv_path]
-        for item in self.list_csv_names.findItems(csv_name, Qt.MatchFlag.MatchExactly):
-            item.setBackground(QBrush(QColor(240, 255, 240)))   # Green
-
-        # Update table if currently selected
-        if self.list_csv_names.currentItem().text() == csv_name:
-            self.update_table(data, csv_path)
+        self.cache[csv_path]['table_data'] = data
+        self.update_table(csv_path)
+        self.paint_list_csv(csv_path, (240, 255, 240))  # Green
 
     def csv_load_failed(self, csv_path):
         # Save in cache
-        self.cache[csv_path] = None
-        self.update_table(None, csv_path)
+        self.cache[csv_path]['table_data'] = None
+        self.update_table(csv_path)
+        self.paint_list_csv(csv_path, (255, 240, 240))  # Red
 
+    def update_table(self, csv_path=""):
+        # Return if not currently selected
         csv_name = self.get_csv_name[csv_path]
-        for item in self.list_csv_names.findItems(csv_name, Qt.MatchFlag.MatchExactly):
-            item.setBackground(QBrush(QColor(255, 240, 240)))   # Red
-
-    def update_table(self, data, csv_path=""):
-        # CSV loading failed
-        if not self.cache[csv_path]:
+        if self.list_csv_names.currentItem().text() != csv_name:
+            return
+        # Return if No data to load
+        if not self.cache[csv_path]['table_data']:
             self.table_csv.setModel(None)
             return
 
-        model = CSVTableModel(data, csv_path)
-        model.load_fail.connect(self.csv_load_failed)
+        # Look if there is model already created
+        if self.cache[csv_path]['table_model']:
+            self.table_csv.setModel(self.cache[csv_path]['table_model'])
+        else:
+            model = CSVTableModel(self.cache[csv_path]['table_data'], csv_path)
+            model.load_fail.connect(self.csv_load_failed)
+            proxy_model = CSVFilterProxyModel()
+            proxy_model.setSourceModel(model)
 
-        self.table_csv.setModel(model)
+            self.cache[csv_path]['table_model'] = proxy_model
+            self.table_csv.setModel(proxy_model)
+
         self.table_csv.horizontalHeader().setDefaultSectionSize(80)     # cell width
         self.table_csv.verticalHeader().setDefaultSectionSize(20)       # cell height
         self.table_csv.scrollTo(self.table_csv.model().index(0, 0), QAbstractItemView.ScrollHint.PositionAtTop)
@@ -181,5 +199,7 @@ class ViewerWindow(QMainWindow, Ui_ViewerWindow):
         for ind in indexes:
             print(ind.row(), ind.column())
 
-
+    def closeEvent(self, event):
+        self.deleteLater()  # 창이 닫힐 때 객체를 완전히 삭제
+        super().closeEvent(event)
 
