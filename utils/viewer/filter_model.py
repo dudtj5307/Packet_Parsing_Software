a1d215af4d8e68d791c1eae65b77705fc55e4686
yslee <yslee5307@gmail.com>
2025-04-03 00:37:22 +0900

    utils/optimizing filtering

--- utils/viewer/filter_model.py
+++ utils/viewer/filter_model.py
@@ -1,24 +1,36 @@
+from collections import defaultdict
+
 from PyQt6.QtCore import QSortFilterProxyModel, Qt
 
 class CSVFilterProxyModel(QSortFilterProxyModel):
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.col_filters = defaultdict(lambda: True)
         self.filter_conditions = {}  # {col: [filter texts]}
+        self.filter_column = 0
 
     def setFilterForColumn(self, column, texts):
-        if texts:
-            self.filter_conditions[column] = [text.lower() for text in texts]
-        else:
-            self.filter_conditions.pop(column, None)
+        # Set
+        self.filter_column = column
+        # Set filtered text to False
+        self.col_filters = defaultdict(lambda: True)
+        self.col_filters.update({text.lower(): False for text in texts})
+        print(self.col_filters)
+
         # Refresh Filter
-        self.invalidateFilter()
+        self.invalidateRowsFilter()
 
     def filterAcceptsRow(self, source_row, source_parent):
         model = self.sourceModel()
-        for column, filter_list in self.filter_conditions.items():
-            index = model.index(source_row, column, source_parent)
-            data = str(model.data(index, role=Qt.ItemDataRole.DisplayRole)).lower()
-            # Any match will be filtered(hidden)
-            if any(filter_text in data for filter_text in filter_list):
-                return False
-        return True
+        index = model.index(source_row, self.filter_column, source_parent)
+
+        # Get the text from the model
+        data_text = str(model.data(index)).lower()
+        return self.col_filters[data_text]  # Will return True by default unless the text is in filtered texts
+
+        # for column, filter_list in self.filter_conditions.items():
+        #     index = model.index(source_row, column, source_parent)
+        #     data = str(model.data(index, role=Qt.ItemDataRole.DisplayRole)).lower()
+        #     # Any match will be filtered(hidden)
+        #     if any(filter_text in data for filter_text in filter_list):
+        #         return False
--- utils/viewer/table_model.py
+++ utils/viewer/table_model.py
@@ -22,15 +22,10 @@ class CSVTableModel(QAbstractTableModel):
     def data(self, index, role=Qt.ItemDataRole.DisplayRole):
         if not self.valid:
             return None
-        try:
-            if role == Qt.ItemDataRole.DisplayRole:
-                return self.csv_data_map.get(index.row())[index.column()]
-            return None
-        except Exception as e:
-            print(f"[Model] Error loading CSV data: {e}")
-            self.valid = False
-            self.load_fail.emit(self.csv_path)
-            return None
+
+        if role == Qt.ItemDataRole.DisplayRole:
+            return self.csv_data_map.get(index.row())[index.column()]
+        return None
 
     def headerData(self, section, orientation, role=Qt.ItemDataRole.DisplayRole):
         if not self.valid:
