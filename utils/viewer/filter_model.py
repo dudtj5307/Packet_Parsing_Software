9f5ad5ba00751450f0d834b429ed98113ff5d161
yslee <yslee5307@gmail.com>
2025-04-03 23:34:10 +0900

    utils/filter_model - increase in filter speed

--- utils/viewer/filter_model.py
+++ utils/viewer/filter_model.py
@@ -11,41 +11,36 @@ class CSVFilterProxyModel(QSortFilterProxyModel):
         self.filtered_texts = set()
         self.cached_data = {}
 
-    def setSourceModel(self, model):
+        self.setDynamicSortFilter(False)
+
+        # Sorting disabled
+        # self.setSortRole(Qt.ItemDataRole.UserRole)
+        # self.sort(-1)
+
+    def setSourceModel(self, model, QAbstractItemModel=None):
         super().setSourceModel(model)
         # Get data from source model
         self.cached_data = model.csv_data_map.copy()  # 얕은 복사
 
-    def setFilterForColumn(self, column, texts):
+    def setFilterForColumn(self, column, hide_texts):
+
         # Set filtering column
         self.filter_column = column
 
-        self.filtered_texts = frozenset(text for text in texts)
+        self.filtered_texts = frozenset(text for text in hide_texts)
 
         # Refresh Filter
-        import time
-        start = time.time()
+        self.blockSignals(True)
         self.invalidateRowsFilter()
-        print("complete time:", time.time() - start)
-
+        self.blockSignals(False)
+        # Update GUI
+        self.layoutChanged.emit()
 
     def filterAcceptsRow(self, source_row, source_parent):
-        # if self.source_model is None:
-        #     return True
-        # index = self.source_model.index(source_row, self.filter_column, source_parent)
-        #
-        # # Get the text from the model
-        # data_text = str(self.source_model.data(index)).lower()
-        # return self.col_filters[data_text]  # Will return True by default unless the text is in filtered texts
-
-        # source_model = self.sourceModel()
-        # index = source_model.index(source_row, self.filter_column, source_parent)
-
-        # data_text = self.cached_data[source_row][self.filter_column]
-
-        # data_text = str(source_model.data(index)).lower()
-
-        if source_row == 0:
-            print(type(self.cached_data[source_row][self.filter_column]), self.filtered_texts)
-
         return self.cached_data[source_row][self.filter_column] not in self.filtered_texts
+
+    def sort(self, column, order=None):
+        pass
+
+    def lessThan(self, left, right):
+        return False
