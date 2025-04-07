741ee2cea6d0564b90a074783e7df0210348462f
yslee <yslee5307@gmail.com>
2025-04-07 21:10:58 +0900

    utils/loader - case where header is smaller

--- utils/viewer/csv_loader.py
+++ utils/viewer/csv_loader.py
@@ -18,6 +18,10 @@ class CSVLoaderThread(QThread):
                     data = [[str(cell) for cell in row] for row in reader]
                 # Data Validation
                 if self.is_valid(data):
+                    header_count, data_count =  len(data[0]), len(data[1])
+                    # If header is shorter
+                    if header_count < data_count:
+                        data[0] += [""] * (data_count-header_count)
                     self.load_complete.emit(self.csv_path, data)
                     print(f"[Loader] Success opening '{self.csv_path}' with {encode_type}")
                     return
@@ -31,4 +35,4 @@ class CSVLoaderThread(QThread):
         print(f"[Loader] Error: Cannot load '{self.csv_path}' with available encodings.\n")
 
     def is_valid(self, data):
-        return all(len(row) == len(data[0]) for row in data)
+        return all(len(row) == len(data[1]) for row in data)
