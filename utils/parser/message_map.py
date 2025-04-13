500e6f07f8bbf83d20a737e9f3c0767a396936cc
yslee <yslee5307@gmail.com>
2025-04-13 23:55:06 +0900

    utils/parser - deleted conversion funcs

--- utils/parser/message_map.py
+++ utils/parser/message_map.py
@@ -15,35 +15,6 @@ TIE_LABEL_NAME = {
     11: 'TEST',     '11': 'TEST',
 }
 
-MDIL_ALPHABETS = 'ABCDEFGHIJKLMNPQ'
-def get_MDIL_STR(num):
-    if type(num) != int: return f"ErrorType({num}({type(num)})";
-    if num > 65535: return f"ErrorNum({num})"
-    if num == 127:
-        return "QH"
-    alpha, octet = divmod(num, 512)
-    alpha1, alpha2 = divmod(alpha, 8)
-    return f"{MDIL_ALPHABETS[alpha1]}{MDIL_ALPHABETS[alpha2]}{octet:03o}"
-
-def get_TDIL_B_STR(num):
-    if type(num) != int: return f"ErrorType({num}({type(num)})";
-    if num > 4095: return f"ErrorNum({num})"
-    return f"{num:04o}"
-
-TDIL_J_ALPHABETS = '01234567ABCDEFGHJKLMNPQRSTUVWXYZ'
-def get_TDIL_J_STR(num):
-    if type(num) != int: return f"ErrorType({num}({type(num)})";
-    if num > 524287: return f"ErrorNum({num})"
-
-    alpha, octet = divmod(num, 512)
-    alpha1, alpha2 = divmod(alpha, 32)
-    return f"{TDIL_J_ALPHABETS[alpha1]}{TDIL_J_ALPHABETS[alpha2]}{octet:03o}"
-
-if __name__ == "__main__":
-    print(get_MDIL_STR(65535))
-    print(get_TDIL_B_STR(4095))
-    print(get_TDIL_J_STR(524287))
-
 
 # if __name__ == '__main__':
 #     for label in range(0, 12):
